b014f0031ff1d0d936e59de6c0eadb26ca5d688e
Wes Hediger <wes.hediger@gmail.com>
2016-11-18 14:50:55 -0700

    add another funtion to multadd

--- Multadd.java
+++ Multadd.java
@@ -9,16 +9,22 @@ public class Multadd {
     **/
 
     public static double multadd(double a, double b, double c){
+      System.out.println(a);
+      System.out.println(b);
+      System.out.println(c);
       return (a * b + c);
     }
 
     public static void main(String[] args){
-      double result = multadd(2.0,3.0,4.0);
+      double result = multadd(1.0,3.0,4.0);
       System.out.println(result);
 
       //find the value of [sin(π/4) + (cos(π/4)/2)] by using multiadd method
-      double trigResult = multadd(Math.cos(Math.PI/4), (double)(1/2), Math.cos(Math.PI/4));
+      double trigResult = multadd(Math.cos(Math.PI/4), (0.5), Math.sin(Math.PI/4));
       System.out.println(trigResult);
+
+      double cos = Math.cos(Math.PI/4)/2 + Math.sin(Math.PI/4);
+      System.out.println(cos);
 
     }
 }
