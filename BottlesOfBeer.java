90ed84a0d5781a2473b8bd139910149dd1cf9033
Wes Hediger <wes.hediger@gmail.com>
2016-11-17 22:50:16 -0700

    refactor beer on the wall

--- BottlesOfBeer.java
+++ BottlesOfBeer.java
@@ -3,21 +3,27 @@ public class BottlesOfBeer {
 
   public void sing(int numOfBeers){
 
-    System.out.println(numOfBeers + " bottles of beer on the wall,");
-    System.out.println(numOfBeers + " bottles of beer,");
-    System.out.println(" ya’ take one down, ya’ pass it around, " + numOfBeers + " bottles of beer on the wall.");
-
+    singVerse(numOfBeers);
     numOfBeers--;
 
     if ( numOfBeers >= 1 ){
       sing(numOfBeers);
     }
     if ( numOfBeers == 0 ){
-      System.out.println("No more bottles of beer on the wall,");
-      System.out.println("No more bottles of beer,");
-      System.out.println("ya’ took them down, ya’ passed them around, now there are no more bottles of beer on the wall!");
+      singLastVerse();
     }
+  }
 
+  public void singVerse(int numOfBeers){
+    System.out.println(numOfBeers + " bottles of beer on the wall,");
+    System.out.println(numOfBeers + " bottles of beer,");
+    System.out.println(" ya’ take one down, ya’ pass it around, " + numOfBeers + " bottles of beer on the wall.");
+  }
+
+  public void singLastVerse(){
+    System.out.println("No more bottles of beer on the wall,");
+    System.out.println("No more bottles of beer,");
+    System.out.println("ya’ took them down, ya’ passed them around, there are no more bottles of beer on the wall!");
   }
 
   public static void main(String[] args){
