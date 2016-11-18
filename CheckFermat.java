41529c96bb9aec3b2f3413876a3f949a27c4f148
Wes Hediger <wes.hediger@gmail.com>
2016-11-17 22:15:44 -0700

    add random numbers to check to Fermat 1000 num batches

--- CheckFermat.java
+++ CheckFermat.java
@@ -2,19 +2,35 @@ import java.lang.Math;
 
 public class CheckFermat {
 
-  public static void checkFermat(int a, int b, int c, int n){
+  public static boolean checkFermat(int a, int b, int c, int n){
     //check: there is no condiiton such that a^n + b^n = c^n except where n >= 2;
+    //must be postive integers
 
     if ( Math.pow(a, n) + Math.pow(b, n) == Math.pow(c, n) && n > 2 ){
-      System.out.println("Fermat was wrong!!!!   +==)========>");
+      System.out.println("Fermat was wrong!!!! HED2084  +==)========>");
+      return true;
     } else if ( Math.pow(a, n) + Math.pow(b, n) == Math.pow(c, n) && n <= 2 ) {
       System.out.println("Yeah, big deal 'n' is less than 2. Fermat is still right.");
+      return false;
     } else {
         System.out.println("howdy pardner");
+        return false;
     }
   }
 
   public static void main(String[] args){
-    checkFermat(1, 2, 3, 4);
+    int a, b, c, n;
+
+    for ( int i = 0; i < 1000; i ++ ){
+      a = (int) Math.random()*10000000;
+      b = (int) Math.random()*10000000;
+      c = (int) Math.random()*10000000;
+      n = 1; //(int) Math.random()*2;
+
+      if(checkFermat(a, b, c, n)){
+        System.out.println("the magic numbers are: a = " + a + " b = " + b + " c = " + c + " n = " + n);
+        break;
+      };
+    }
   }
 }
