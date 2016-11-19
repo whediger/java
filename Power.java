c29c02f4a98422d86c9a5d6f1c835adbf9592cb5
Wes Hediger <wes.hediger@gmail.com>
2016-11-18 20:51:07 -0700

    add iterative version of power

--- Power.java
+++ Power.java
@@ -13,10 +13,21 @@ public class Power {
     return power(x, (n - 1), (x * total));
   }
 
+  public static double iterativePower(double x, int n){
+    double num = x;
+    for (int i = n -1; i > 0; i--){
+      x *= num;
+    }
+    return x;
+  }
+
 
   public static void main(String[] args){
 
     double result = power(5, 4);
     System.out.println(result);
+
+    double iterate = iterativePower(5, 4);
+    System.out.println(iterate);
   }
 }
