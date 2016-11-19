6418e82e6aeac537e5d289cde4062c4860797864
Wes Hediger <wes.hediger@gmail.com>
2016-11-18 23:40:01 -0700

    generalize powArray

--- PowArray.java
+++ PowArray.java
@@ -2,10 +2,10 @@ import java.lang.Math;
 
 public class PowArray {
 
-  public static double[] powArray(double[] a){
+  public static double[] powArray(double[] a, int n){
 
     for (double i : a) {
-      i = Math.pow(i, 2);
+      i = Math.pow(i, n);
       System.out.print(i + ", ");
     }
     return a;
@@ -14,6 +14,6 @@ public class PowArray {
   public static void main(String[] args){
 
     double[] a = { 2, 3, 4, 6, 1, 3, 7, 6 };
-    a = powArray(a);
+    a = powArray(a, 2);
   }
 }
