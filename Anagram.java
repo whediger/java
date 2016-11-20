6f582e10f73acd929a8543de93fef4cd8ab63248
Wes Hediger <wes.hediger@gmail.com>
2016-11-19 23:23:12 -0700

    add anagram

---
+++ Anagram.java
@@ -0,0 +1,50 @@
+
+public class Anagram {
+
+  public static boolean anagram(String str1, String str2){
+
+    str1 = str1.replaceAll( "[ ]+", "" );
+    str2 = str2.replaceAll( "[ ]+", "" );
+
+    if ( str1.length() != str2.length()) { return false; }
+    int length = str1.length() -1;
+    System.out.println(str1);
+    System.out.println(str2);
+
+    return letterMatch(str1, str2, length);
+  }
+
+  private static boolean letterMatch(String str1, String str2, int charCount){
+
+    int str1length = str1.replaceAll( "[^" + str1.charAt(charCount) + "]", "" ).length();
+    int str2length = str2.replaceAll( "[^" + str1.charAt(charCount) + "]", "" ).length();
+
+    System.out.println("str1 length " + str1length);
+    System.out.println(str1.charAt(charCount));
+    System.out.println("str2 length " + str2length);
+    System.out.println(str2.charAt(charCount));
+
+    if (str1length != str2length) { return false; };
+    if (charCount == 0 ) { return true; };
+
+    charCount--;
+    return letterMatch(str1, str2, charCount);
+  }
+
+  public static void main(String[] args){
+    //true
+    String str1 = "allen downey";
+    String str2 = "well annoyed";
+    //false
+    String str3 = "discombobulated";
+    String str4 = "well annoyeddddd";
+
+    boolean result = anagram(str1, str2);
+    System.out.println("true result: " + result);
+
+    System.out.println("  +==]========>");
+
+    result = anagram(str3, str4);
+    System.out.println("false result: " + result);
+  }
+}
