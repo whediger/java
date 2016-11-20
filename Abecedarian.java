11b42e01e151d027f73b0279bd55213aa804b47f
Wes Hediger <wes.hediger@gmail.com>
2016-11-19 17:56:09 -0700

    add abecedarian

---
+++ Abecedarian.java
@@ -0,0 +1,38 @@
+
+public class Abecedarian {
+
+  public static boolean abecedarian(String word){
+    Character[] abc = new Character[26];
+    int i = 0;
+    for (char c = 'a'; c <= 'z'; c++){
+      abc[i] = c;
+      i++;
+    }
+
+    int letterShift = 0;
+    for ( int e = 0; e < abc.length; e++){
+      if (word.charAt(letterShift) == abc[e]) {
+        letterShift++;
+      }
+      if (letterShift >= word.length()){
+        break;
+      }
+      if (word.length() - letterShift + 1 > abc.length - e){
+        return false;
+      }
+    }
+    return true;
+  }
+  
+  public static void main(String[] args){
+
+    //true test
+    boolean test = abecedarian("abejoptvz");
+    System.out.println(test);
+
+    //false test
+    test = abecedarian("abejoptavz");
+    System.out.println(test);
+
+  }
+}
