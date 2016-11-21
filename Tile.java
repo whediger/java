037e71e2ccf63fc57692390516a96dec553761f2
Wes Hediger <wes.hediger@gmail.com>
2016-11-21 14:22:44 -0700

    add testTile to Tile

--- Tile.java
+++ Tile.java
@@ -11,12 +11,17 @@ public class Tile {
 
   public static void printTile(Tile thisTile){
     System.out.print("This tile: '" + thisTile.letter + "' value: " + thisTile.value);
+    System.out.println("");
+  }
+
+  public static void testTile(){
+    Tile testT = new Tile('Z', 10);
+    printTile(testT);
   }
 
   public static void main(String[] args){
-
-    Tile myTile = new Tile('a', 1);
+    Tile myTile = new Tile('A', 1);
     printTile(myTile);
-
+    testTile();
   }
 }
