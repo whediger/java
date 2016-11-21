0e8ca1979c71efdf3a980c45cdf10c700d9fe574
Wes Hediger <wes.hediger@gmail.com>
2016-11-21 14:48:49 -0700

    add getters and setters

--- Tile.java
+++ Tile.java
@@ -1,12 +1,28 @@
 
 public class Tile {
 
-  char letter;
-  int value;
+  private char letter;
+  private int value;
 
   public Tile(char letter, int value){
     this.letter = letter;
      this.value = value;
+  }
+
+  public char getLetter(){
+    return this.letter;
+  }
+
+  public int getValue(){
+    return this.value;
+  }
+
+  public void setLetter(char letter){
+    this.letter = letter;
+  }
+
+  public void setValue(int value){
+    this.value = value;
   }
 
   public static void printTile(Tile thisTile){
