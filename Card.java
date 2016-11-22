c691d18266966f654110fb028b72b4a76d3c11eb
Wes Hediger <wes.hediger@gmail.com>
2016-11-21 23:00:49 -0700

    add getters to Cards

--- Card.java
+++ Card.java
@@ -17,20 +17,42 @@ public class Card {
   }
 
   // +==]========>
+  public int getRank(){
+    return this.rank;
+  }
+
+  public int getsuit(){
+    return this.suit;
+  }
+
   public String toString() {
     return RANKS[this.rank] + " of " + SUITS[this.suit];
   }
 
   public boolean equals(Card that) {
     return this.rank == that.rank
+      && this.suit == that.suit;
   }
 
   public String toString(){
-    String[] ranks = { null, "Ace", "2", "3", "4", "5", "6",
-               "7", "8", "9", "10", "Jack", "Queen", "King" };
-    String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"}
     String s = ranks[this.rank] + " of " + suits[this.suit];
     return s;
+  }
+
+  public int compareTo(Card that){
+    if (this.suit < that.suit){
+      return -1;
+    }
+    if (this.suit > that.suit){
+      return 1;
+    }
+    if (this.rank < that.rank){
+      return -1;
+    }
+    if (this.rank > that.rank){
+      return 1;
+    }
+    return 0;
   }
 
   // +==]========>
