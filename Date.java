e7f8f9c965eb00cfb7c5f45ed3b3094bb27f3bcd
Wes Hediger <wes.hediger@gmail.com>
2016-11-21 18:32:44 -0700

    add ocnstructors to Date

--- Date.java
+++ Date.java
@@ -1,22 +1,30 @@
 
-public class Time {
 
 
+public class Date {
+
+  private int year;
+  private int month;
+  private int day;
+
+
+  public Date(){
+    this.year = 2016;
+    this.month = 11;
+    this.day = 15;
+  }
+
+  public Date(int year, int month, int day){
+    this.year = year;
+    this.month = month;
+    this.day = day;
+  }
 
 
   public static void main(String[] args){
-
-    String day = "";
-    Integer date = 0;
-    String month = "";
-    Integer year = 0;
-
-    day = "Thursday";
-    date = 14;
-    month = "November";
-    year = 2016;
-
-    System.out.println(day + ", " + month + " " + date + ", " + year);
-
+    Date today = new Date();
+    System.out.println(today.year + " " + today.month + " " + today.day);
+    Date newDate = new Date(2016, 6, 3);
+    System.out.println(newDate.year + " " + newDate.month + " " + newDate.day);
   }
 }
