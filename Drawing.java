dc19af4ab92603a3408a54bfd8590a0aad2a12a0
Wes Hediger <wes.hediger@gmail.com>
2016-11-22 17:17:09 -0700

    add Japan flag to drawing

--- Drawing.java
+++ Drawing.java
@@ -9,34 +9,17 @@ public class Drawing extends Canvas {
 
   public void paint(Graphics g) {
     Color myBlue = new Color(0,162,232);
-    g.setColor(Color.blue);
-    g.fillOval(100, 100, 200, 200);
+    g.setColor(Color.red);
+    g.fillOval(200, 100, 200, 200);
 
-  }
-  public void boxOval(Graphics g, Rectangle bb){
-    g.fillOval(bb.x, bb.y, bb.width, bb.height);
-  }
-
-  public void micky(Graphics g, Rectangle bb){
-    boxOval(g, bb);
-
-    int dx = bb.width / 2;
-    int dy = bb.height / 2;
-    Rectangle half = new Rectangle(bb.x, bb.y, dx, dy);
-
-    half.translate(-dx / 2, -dy / 2);
-    boxOval(g, half);
-
-    half.translate(dx * 2, 0);
-    boxOval(g, half);
   }
 
   //  +==]========>
   public static void main(String[] args){
     JFrame frame = new JFrame("my drawing");
     Canvas canvas = new Drawing();
-    canvas.setSize(400, 400);
-    canvas.setBackground(Color.darkGray);
+    canvas.setSize(600, 400);
+    canvas.setBackground(Color.white);
     frame.add(canvas);
     frame.pack();
     frame.setVisible(true);
