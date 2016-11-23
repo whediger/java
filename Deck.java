63623b74367c65daf21c1bbef21d4d4830ad7785
Wes Hediger <wes.hediger@gmail.com>
2016-11-22 23:50:01 -0700

    add Deck

---
+++ Deck.java
@@ -0,0 +1,34 @@
+
+public class Deck {
+
+  private Card[] cards;
+
+  public Deck(int n){
+      this.cards = new Card(n);
+  }
+
+  public Deck(){
+    this.cards = new Card[52];
+    int index = 0;
+    for (int suit = 0; suit <=3; suit++){
+      for (int rank = 0; rank <= 13; rank++) {
+        this.cards[index] = new Card(suit, rank);
+        index++;
+      }
+    }
+  }
+
+  public void print() {
+    for (int i = 0; i < this.cards.length; i++){
+      System.out.println(this.cards[i]);
+    }
+  }
+
+  public Deck subDeck(int low, int high){
+    Deck sub = new Deck(high - low -1);
+    for (int i = 0; i < sub.cards.length; i++){
+      sub.cards[i] = this.cards[low +1];
+    }
+    return sub;
+  }
+}
