f0811eb5909f36a9ba4edccf90bfe449f383b504
Wes Hediger <wes.hediger@gmail.com>
2016-11-29 11:37:37 -0700

    add swap cards

--- CardCollection.java
+++ CardCollection.java
@@ -54,4 +54,10 @@ public class CardCollection {
       that.addCard(card);
     }
   }
+
+  public void swapCards(int i, int j){
+    Card temp = cards.get(i);
+    cards.set(i, cards.get(j));
+    cards.set(j, temp);
+  }
 }
