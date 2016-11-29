873ac8cdbffe278e937b19046ff6a5445bcbb0f4
Wes Hediger <wes.hediger@gmail.com>
2016-11-29 11:02:19 -0700

    CardCollection.java initial commit

---
+++ CardCollection.java
@@ -0,0 +1,11 @@
+
+public class CardCollection {
+
+  private String label;
+  private ArrayList<Card> cards;
+
+  private CardCollection(String label){
+    this.label = lable;
+    this.cards = new ArrayList<Card>();
+  }
+}
