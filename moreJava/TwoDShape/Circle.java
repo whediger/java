2865e1a8288bc14f3fb3e81163a475a1285f53f2
Wes Hediger <wes.hediger@gmail.com>
2016-11-30 16:14:15 -0700

    add Circle

---
+++ moreJava/TwoDShape/Circle.java
@@ -0,0 +1,31 @@
+import java.lang.Math;
+
+
+class Circle extends TwoDShape {
+  Circle(){
+    super();
+  }
+
+  Circle(double w, double h, String n) {
+    super(w, h, "circle");
+  }
+
+  Circle(double x, String n){
+    super(x, "circle");
+  }
+
+  Circle(TwoDShape ob){
+    super(ob);
+  }
+
+  public boolean isCircle() {
+    return getWidth() == getHeight();
+  }
+
+  double area() {
+    if (getWidth() == getHeight()) {
+      return Math.PI * (getWidth() / 2) * (getWidth() / 2);
+    }
+    return Math.PI * (getWidth() / 2) * (getHeight() / 2);
+  }
+}
