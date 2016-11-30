67f016d7c40a858a587468256fa81314bd48cb58
Wes Hediger <wes.hediger@gmail.com>
2016-11-30 15:52:19 -0700

    remove generic instance from DynShapes

--- moreJava/TwoDShape/DynShapes.java
+++ moreJava/TwoDShape/DynShapes.java
@@ -6,7 +6,6 @@ class DynShapes {
     shapes[1] = new Rectangle(10);
     shapes[2] = new Rectangle(10, 4);
     shapes[3] = new Triangle(7.0);
-    shapes[4] = new TwoDShape(10, 20, "generic");
 
     for (int i = 0; i < shapes.length; i++) {
       System.out.println("object is " + shapes[i].getName());
