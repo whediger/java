dda92d64aff9449578ff21ac7d854431c2728323
Wes Hediger <wes.hediger@gmail.com>
2016-12-01 16:02:42 -0700

    add ByThrees to SeriesDemo

--- moreJava/series/SeriesDemo.java
+++ moreJava/series/SeriesDemo.java
@@ -17,5 +17,23 @@ class SeriesDemo {
     ob.setStart(100);
     for (int i = 1; i < 100; i++)
       System.out.println("Next value is: " + ob.getNext());
+
+System.out.println("BYThrees  +==]========>");
+
+    ByThrees ob3 = new ByThrees();
+
+    for (int i = 0; i < 5; i++) {
+      System.out.println("Next value is: " + ob3.getNext());
+    }
+
+    System.out.println("\nResetting");
+    ob3.reset();
+    for (int i = 0; i < 5; i++)
+      System.out.println("Next value is: " + ob3.getNext());
+
+    System.out.println("\nStarting at 100");
+    ob3.setStart(100);
+    for (int i = 1; i < 100; i++)
+      System.out.println("Next value is: " + ob3.getNext());
   }
 }
