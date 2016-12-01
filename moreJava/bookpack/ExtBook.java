989d1fbb6e05271c3b6f180b3560bff67e1c7b69
Wes Hediger <wes.hediger@gmail.com>
2016-12-01 12:11:18 -0700

    add ProtectDemo

--- moreJava/bookpack/ExtBook.java
+++ moreJava/bookpack/ExtBook.java
@@ -25,3 +25,16 @@ class ExtBook extends bookpack.Book {
   public int getPubDate() { return pubDate; }
   public void setPubDate(int p) { pubDate = p; }
 }
+
+class ProtectDemo {
+  public static void main(String[] args){
+    ExtBook books[] = new ExtBook[5];
+    books[0] = new ExtBook("Java: A Beginner's Guide", "Schildt", 2014, "McGraw-Hill Professional");
+    books[1] = new ExtBook("Java: The compleate reference", "Schildt", 2014, "McGraw-Hill Professional");
+    books[2] = new ExtBook("the Art of Java", "Schildt and Holmes", 2003, "McGraw-Hill Professional");
+    books[3] = new ExtBook("Red Storm Rising", "Clancy", 1986, "Putnam");
+    books[4] = new ExtBook("On the Road", "Kerouac", 1955, "Viking");
+
+    for (int i = 0; i < books.length; i++) books[i].show();
+  }
+}
