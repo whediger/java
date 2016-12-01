b93f1c7128d3d900e3b5361c6c8082d8dbf7ba37
Wes Hediger <wes.hediger@gmail.com>
2016-12-01 11:48:30 -0700

    add class bookDemo

--- moreJava/bookpack/BookDemo.java
+++ moreJava/bookpack/BookDemo.java
@@ -3,7 +3,7 @@ package bookPack;
 class Book {
   private String title;
   private String author;
-  private String pubDate;
+  private int pubDate;
 
   Book(String t, String a, int d){
     title = t;
@@ -16,5 +16,19 @@ class Book {
     System.out.println(author);
     System.out.println(pubDate);
     System.out.println();
+  }
+
+  class BookDemo {
+    public static void main(String[] args) {
+      Book books[] = new Book[5];
+
+      books[0] = new Book("Java: A Beginners Guide", "Schildt", 2014);
+      books[1] = new Book("Java: The Compleate Reference", "Schildt", 2014);
+      books[2] = new Book("The Art of Java", "Schildt and Holmes", 2003);
+      books[3] = new Book("Red Storm Rising", "Clancy", 1986);
+      books[4] = new Book("On the Road", "Kerouac", 1955);
+
+      for (int i = 0; i < books.length; i++) books.show();
+    }
   }
 }
