3f6887f7a660b9fb2374da5e3348dd9d382bb721
Wes Hediger <wes.hediger@gmail.com>
2016-12-01 16:15:09 -0700

    add interface instance test to SeriesDemo

--- moreJava/series/SeriesDemo.java
+++ moreJava/series/SeriesDemo.java
@@ -2,38 +2,52 @@
 class SeriesDemo {
 
   public static void main(String args[]){
-    ByTwos ob = new ByTwos();
+//     ByTwos ob = new ByTwos();
+//
+//     for (int i = 0; i < 5; i++) {
+//       System.out.println("Next value is: " + ob.getNext());
+//     }
+//
+//     System.out.println("\nResetting");
+//     ob.reset();
+//     for (int i = 0; i < 5; i++)
+//       System.out.println("Next value is: " + ob.getNext());
+//
+//     System.out.println("\nStarting at 100");
+//     ob.setStart(100);
+//     for (int i = 1; i < 100; i++)
+//       System.out.println("Next value is: " + ob.getNext());
+//
+// System.out.println("ByThrees  +==]========>");
+//
+//     ByThrees ob3 = new ByThrees();
+//
+//     for (int i = 0; i < 5; i++) {
+//       System.out.println("Next value is: " + ob3.getNext());
+//     }
+//
+//     System.out.println("\nResetting");
+//     ob3.reset();
+//     for (int i = 0; i < 5; i++)
+//       System.out.println("Next value is: " + ob3.getNext());
+//
+//     System.out.println("\nStarting at 100");
+//     ob3.setStart(100);
+//     for (int i = 1; i < 100; i++)
+//       System.out.println("Next value is: " + ob3.getNext());
+//
+
+  // using an interface reference  +==]========>
+    ByTwos twoOb = new ByTwos();
+    ByThrees threeOb = new ByThrees();
+    Series ob;
 
     for (int i = 0; i < 5; i++) {
-      System.out.println("Next value is: " + ob.getNext());
+      ob = twoOb;
+      System.out.println("Next ByTwos value is: " + ob.getNext());
+
+      ob = threeOb;
+      System.out.println("Next ByThrees vlaue is: " + ob.getNext());
     }
-
-    System.out.println("\nResetting");
-    ob.reset();
-    for (int i = 0; i < 5; i++)
-      System.out.println("Next value is: " + ob.getNext());
-
-    System.out.println("\nStarting at 100");
-    ob.setStart(100);
-    for (int i = 1; i < 100; i++)
-      System.out.println("Next value is: " + ob.getNext());
-
-System.out.println("BYThrees  +==]========>");
-
-    ByThrees ob3 = new ByThrees();
-
-    for (int i = 0; i < 5; i++) {
-      System.out.println("Next value is: " + ob3.getNext());
-    }
-
-    System.out.println("\nResetting");
-    ob3.reset();
-    for (int i = 0; i < 5; i++)
-      System.out.println("Next value is: " + ob3.getNext());
-
-    System.out.println("\nStarting at 100");
-    ob3.setStart(100);
-    for (int i = 1; i < 100; i++)
-      System.out.println("Next value is: " + ob3.getNext());
   }
 }
