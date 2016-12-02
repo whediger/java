bf032a0b659e922ec5008c7234a6526f149fe831
Wes Hediger <wes.hediger@gmail.com>
2016-12-02 13:44:15 -0700

    add CustomExeptDemo

---
+++ moreJava/excexorcises/CustomExceptDemo.java
@@ -0,0 +1,43 @@
+
+//Use a custom exception
+
+//create exception
+class NonIntResultException extends Exception {
+  int n;
+  int d;
+
+  NonIntResultException(int i, int j){
+    n = i;
+    d = j;
+  }
+
+  public String toString() {
+    return "Result of " + n + " / " + d + " is non-integer";
+  }
+}
+
+class CustomExceptDemo {
+  public static void main(String args[]){
+    //numer contains odd values
+    int numer[] = { 4, 8, 15, 32, 64, 127, 256, 512 };
+    int denom[] = { 2, 0, 4, 4, 0, 8 };
+
+    for (int i = 0; i < numer.length; i++) {
+      try {
+        if ((numer[i]%2) != 0)
+            throw new
+              NonIntResultException(numer[i], denom[i]);
+
+        System.out.println(numer[i] + " / "
+                            + denom[i]
+                            + " is " + numer[i]/denom[i]);
+      } catch (ArithmeticException exc) {
+        System.out.println("can't divide by zero");
+      } catch (ArrayIndexOutOfBoundsException exc){
+        System.out.println("No matching element found.");
+      } catch (NonIntResultException exc){
+        System.out.println(exc);
+      }
+    }
+  }
+}
