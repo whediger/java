3b75c6a300cabb3f10f1fb1cbbfd135a5945a611
Wes Hediger <wes.hediger@gmail.com>
2016-12-01 21:11:37 -0700

    add CircularQueue

--- moreJava/queue/IQDemo.java
+++ moreJava/queue/IQDemo.java
@@ -24,3 +24,33 @@ class FixedQueue implements ICharQ {
     return q[getLoc++];
   }
 }
+
+class CircularQueue implements ICharQ {
+  private char q[];//holds the queue
+  private int putLoc, getLoc; //get and put indices
+
+  public CircularQueue(int size){
+    q = new char[size+1]; //alocate memory for queue
+    putLoc = getLoc = 0;
+  }
+
+  public void put(char ch){
+    if(putLoc+1 == getLoc | ((putLoc == q.length-1) & (getLoc == 0))) {
+      System.out.println(" - Queue is full.");
+      return;
+    }
+    q[putLoc++] = ch;
+    if(putLoc == q.length) putLoc = 0; //loop back
+  }
+
+  public char get(){
+    if(getLoc == putLoc){
+      System.out.println(" - Queue is empty.");
+      return (char) 0;
+    }
+
+      char ch = q[getLoc++];
+      if(getLoc == q.length) getLoc = 0;
+      return ch;
+  }
+}
