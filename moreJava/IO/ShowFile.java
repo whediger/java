345327b6239a11896599978b2e3e02a7edc35f68
Wes Hediger <wes.hediger@gmail.com>
2016-12-02 18:55:49 -0700

    add ShowFile

---
+++ moreJava/IO/ShowFile.java
@@ -0,0 +1,41 @@
+import java.io.*;
+
+class ShowFile {
+  public static void main(String args[])
+  {
+      int i;
+      FileInputStream fin;
+
+      //first make sure file has been specified
+      if (args.length != 1) {
+        System.out.println("Usage: ShowFile File.");
+        return;
+      }
+
+      try {
+        fin = new FileInputStream(args[0]);
+      } catch (FileNotFoundException exc) {
+        System.out.println("File Not Found");
+        return;
+      }
+
+      try {
+        //read bytes until EOF is encountered
+        do {
+          i = fin.read();
+          if(i != -1) System.out.print((char) i);
+        } while (i != -1);
+        System.out.println();
+      } catch (IOException exc) {
+        System.out.println(("Error reading file."));
+      } finally {
+        try {
+          fin.close();
+        } catch (IOException exc) {
+          System.out.println("Error closing file.");
+        }
+      }
+
+
+  }
+}
