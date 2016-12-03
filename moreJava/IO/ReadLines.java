bc9a48767b54ebe5a0512d7334cf59f49512633f
Wes Hediger <wes.hediger@gmail.com>
2016-12-03 15:10:35 -0700

    add ReadLines

---
+++ moreJava/IO/ReadLines.java
@@ -0,0 +1,20 @@
+//read a string from console using a BufferedReader
+import java.io.*;
+
+class ReadLines {
+  public static void main(String args[])
+    throws IOException
+  {
+    //create buffered reader using System.in
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    String str;
+
+    System.out.println("Enter lines of text");
+    System.out.println("Enter 'stop' to quit.");
+    do {
+      str = br.readLine();
+      System.out.println(str);
+    } while (!str.equals("stop"));
+  }
+}
