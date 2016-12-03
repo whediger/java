37e982896c703a14d76aeb07d4001385750b0f42
Wes Hediger <wes.hediger@gmail.com>
2016-12-02 20:07:24 -0700

    refactor CopyFile to use try-with-resources

--- moreJava/IO/CopyFile.java
+++ moreJava/IO/CopyFile.java
@@ -4,8 +4,6 @@ class CopyFile {
   public static void main(String args[]) throws IOException
   {
     int i;
-    FileInputStream fin = null;
-    FileOutputStream fout= null;
 
     //make sure both files have benn specified
     if (args.length != 2) {
@@ -14,28 +12,16 @@ class CopyFile {
     }
 
     //Copy a file
-    try {
-      // attempt to open Files`
-      fin = new FileInputStream(args[0]);
-      fout = new FileOutputStream(args[1]);
+    try (FileInputStream fin = new FileInputStream(args[0]);
+          FileOutputStream fout = new FileOutputStream(args[1])) {
 
       do {
         i = fin.read();
         if(i != -1) fout.write(i);
       } while(i != -1);
+
     } catch (IOException exc){
       System.out.println("I/O Error: " + exc);
-    } finally {
-      try {
-        if (fin != null) fin.close();
-      } catch (IOException exc){
-        System.out.println("Error closing input file.");
-      }
-      try {
-        if(fout != null) fout.close();
-      } catch (IOException exc) {
-        System.out.println("Error Closing output file.");
-      }
-    }
+    } 
   }
 }
