06e04c107f4053320a6d816d9d0821f17e50e86b
Wes Hediger <wes.hediger@gmail.com>
2016-12-06 12:19:44 -0700

    add AWT applet template

---
+++ moreJava/misc/appletTemplate-AWT-based.java
@@ -0,0 +1,35 @@
+//AWT-based Applet skeleton
+
+import java.awt.*;
+import java.applet.*;
+
+/*
+<applet code="AppletSkel" width=300 height=100></applet>
+*/
+
+public class AppletSkel extends Applet {
+  //called first
+  public void init() {
+    //initialization
+  }
+
+  //called second, after init(). also called when restarted
+  public void start() {
+    //start or resume exicution
+  }
+
+  //called when applet stopped. see destroy to terminate
+  public void stop(){
+    //suspends execution
+  }
+
+  //called when applet terminated
+  public void destroy(){
+    //perform shutdown activities
+  }
+
+  //called when an AWT-based applet iwndow must be restored
+  public void paint(Graphics g ){
+    //redisplay contents of window
+  }
+}
