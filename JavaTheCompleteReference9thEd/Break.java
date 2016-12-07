32020a8c53824ceb6971419a0db8aa3a93331915
Wes Hediger <wes.hediger@gmail.com>
2016-12-07 14:18:12 -0700

    add example of breeaking out of outerloop from inner loop

--- JavaTheCompleteReference9thEd/Break.java
+++ JavaTheCompleteReference9thEd/Break.java
@@ -1,4 +1,5 @@
 //uses new age goto syntax
+//>>>>>>>>use for breaking out of nested loops <<<<<<<<<<<<<<<<<<<<
 
 class Break {
   public static void main(String args[])
@@ -15,6 +16,19 @@ class Break {
       System.out.println("this will not exicute");
       } //second
     System.out.println("hello after second block");
+    }
+    System.out.println();
+    System.out.println(" +==]========>");
+    System.out.println();
+
+    //========================================
+    //break out of outer loop from inner loops
+    //========================================
+    outer: for (int i = 0; i < 3; i++) {
+      System.out.print("Pass " + i + ": ");
+      for (int j = 0; j < 100; j++) {
+        if(j == 10) break outer; //breaks out of both loops
+      }
     }
   }
 }
