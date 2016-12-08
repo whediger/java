b289cbbc8d3cdc8fdd45bd9819a4e1aab8f3cef6
Wes Hediger <wes.hediger@gmail.com>
2016-12-08 12:16:19 -0700

    add SamePackage

---
+++ JavaTheCompleteReference9thEd/ch9/p1/SamePackage.java
@@ -0,0 +1,17 @@
+
+package p1;
+
+class SamePackage {
+  SamePackage() {
+
+    Protection p = new Protection();
+
+    System.out.println("same package contructor");
+    System.out.println(" n = " = p.n);
+
+    //private is class only
+
+    System.out.println("n_pro = " + n_pro);
+    System.out.println("n_pub = " + n_pub);
+  }
+}
