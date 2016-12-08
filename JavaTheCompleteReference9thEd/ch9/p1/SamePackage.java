4c0479cec433d24c2e3330be9da628fa4690b023
Wes Hediger <wes.hediger@gmail.com>
2016-12-08 12:53:12 -0700

    add p2.Protection2

--- JavaTheCompleteReference9thEd/ch9/p1/SamePackage.java
+++ JavaTheCompleteReference9thEd/ch9/p1/SamePackage.java
@@ -7,11 +7,11 @@ class SamePackage {
     Protection p = new Protection();
 
     System.out.println("same package contructor");
-    System.out.println(" n = " = p.n);
+    System.out.println(" n = " + p.n);
 
     //private is class only
 
-    System.out.println("n_pro = " + n_pro);
-    System.out.println("n_pub = " + n_pub);
+    System.out.println("n_pro = " + p.n_pro);
+    System.out.println("n_pub = " + p.n_pub);
   }
 }
---
+++ JavaTheCompleteReference9thEd/ch9/p2/Protection2.java
@@ -0,0 +1,15 @@
+
+package p2;
+
+class Protection2 extends p1.Protection {
+  Protection2(){
+    System.out.println("derived other package constructor");
+
+    //n is class or package only
+
+    //n_pri is class only
+
+    System.out.println("n_pro = " + n_pro);
+    System.out.println("n_pub = " + n_pub);
+  }
+}
