a50cf33d9ed38a3a9675b9701ba3adda2219ecdf
Wes Hediger <wes.hediger@gmail.com>
2016-12-08 13:02:10 -0700

    add p1.Demo

---
+++ JavaTheCompleteReference9thEd/ch9/p1/Demo.java
@@ -0,0 +1,11 @@
+
+package p1;
+
+public class Demo {
+  public static void main(String args[])
+  {
+    Protection ob1 = new Protection();
+    Derived d1 = new Derived();
+    SamePackage ob3 = new SamePackage();
+  }
+}
