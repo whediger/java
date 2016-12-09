982fd559ee2ad8edd3c6636dae8570fd99c8f86f
Wes Hediger <wes.hediger@gmail.com>
2016-12-08 19:12:01 -0700

    add Synch

---
+++ JavaTheCompleteReference9thEd/ch11/Synch.java
@@ -0,0 +1,47 @@
+
+class CallMe {
+  void call(String msg){
+    System.out.println("[" + msg);
+    try {
+      Thread.sleep(1000);
+    } catch (InterruptedException exc){
+      System.out.println("Interrupted");
+    }
+    System.out.println("]");
+  }
+}
+
+class Caller implements Runnable {
+  String msg;
+  CallMe target;
+  Thread t;
+
+  public Caller(CallMe targ, String s){
+    target = targ;
+    msg = s;
+    t = new Thread(this);
+    t.start();
+  }
+  public void run(){
+    target.call(msg);
+  }
+}
+
+class Synch {
+  public static void main(String args[])
+  {
+    CallMe target = new CallMe();
+    Caller ob1 = new Caller(target, "Hello");
+    Caller ob2 = new Caller(target, "Synchronized");
+    Caller ob3 = new Caller(target, "World");
+
+    //wait for threads to end
+    try {
+      ob1.t.join();
+      ob2.t.join();
+      ob3.t.join();
+    } catch (InterruptedException exc) {
+      System.out.println("interruption in main");
+    }
+  }
+}
