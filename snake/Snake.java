ea7a2ef27b63ff14f180de9a3ff4407c3c645124
Wes Hediger <wes.hediger@gmail.com>
2016-12-10 13:14:54 -0700

    add drawCircle random x, y values

--- snake/Snake.java
+++ snake/Snake.java
@@ -4,21 +4,33 @@ package snake;
 import java.awt.*;
 import java.awt.Canvas;
 import javax.swing.JFrame;
+import java.util.Random;
 
 public class Snake extends Canvas{
+
+  static final int FRAME_HEIGHT = 500;
+  static final int FRAME_WIDTH = 500;
 
   public static void main(String args[])
   {
     JFrame f = new JFrame("Snake Game");
     Canvas c = new Canvas();
-    f.setSize(300, 300);
+    f.setSize(FRAME_HEIGHT, FRAME_WIDTH);
     f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    c.setSize(300, 300);
+
+    c.setSize(FRAME_HEIGHT, FRAME_WIDTH);
     c.setBackground(Color.GREEN);
+
     f.add(c);
     f.setVisible(true);
+    drawCircle();
+  }
 
-
+  public static void drawCircle() {
+    Random rand = new Random();
+    int y = rand.nextInt(FRAME_HEIGHT);
+    int x = rand.nextInt(FRAME_WIDTH);
+    System.out.println("x: " + x + " y: " + y);
   }
 
 }
