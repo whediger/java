83efe73b11e7bf267716ee79687d99f509fb6b47
Wes Hediger <wes.hediger@gmail.com>
2016-12-15 16:54:24 -0700

    add capacity comment

--- JavaTheCompleteReference9thEd/Part-II-TheJavaLibrary/ch16/StringBufferDemo.java
+++ JavaTheCompleteReference9thEd/Part-II-TheJavaLibrary/ch16/StringBufferDemo.java
@@ -8,6 +8,7 @@ class StringBufferDemo {
 
     System.out.println("Buffer = " + sb);
     System.out.println("length = " + sb.length());
+    //capacity is 21 because 16 additional chars are automatically added
     System.out.println("capacity = " + sb.capacity());
   }
 }
