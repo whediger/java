ff1beb6702c33f412c47c60722b9006008668e81
Wes Hediger <wes.hediger@gmail.com>
2016-12-15 15:14:58 -0700

    add EqualsDemo

---
+++ JavaTheCompleteReference9thEd/Part-II-TheJavaLibrary/ch16/EqualsDemo.java
@@ -0,0 +1,15 @@
+
+class EqualsDemo {
+  public static void main(String args[])
+  {
+    String s1 = "Hello";
+    String s2 = "Goodbye";
+    String s3 = "Hello";
+    String s4 = "HELLO";
+
+    System.out.println("s1 equals s2 " + s1.equals(s2));
+    System.out.println("s1 equals s3 " + s1.equals(s3));
+    System.out.println("s1 equals s4 " + s1.equals(s4));
+    System.out.println("s1 equals s4 ignoreCase " + s1.equalsIgnoreCase(s4));
+  }
+}
