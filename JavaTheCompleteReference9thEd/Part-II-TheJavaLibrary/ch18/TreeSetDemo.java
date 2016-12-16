e917b7c93b4d708c9a3c9ccd7de6cb6278d4e019
Wes Hediger <wes.hediger@gmail.com>
2016-12-16 13:41:29 -0700

    add TreeSetDemo

---
+++ JavaTheCompleteReference9thEd/Part-II-TheJavaLibrary/ch18/TreeSetDemo.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+
+class TreeSetDemo {
+  public static void main(String args[])
+  {
+    TreeSet<String> ts = new TreeSet<String>();
+
+    ts.add("A");
+    ts.add("B");
+    ts.add("C");
+    ts.add("D");
+    ts.add("E");
+    ts.add("F");
+
+    System.out.println(ts);
+  }
+}
