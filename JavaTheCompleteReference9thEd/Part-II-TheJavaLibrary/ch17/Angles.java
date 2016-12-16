deab1255fcfe7d47b3603fa3a2b6d2bff865c431
Wes Hediger <wes.hediger@gmail.com>
2016-12-15 21:41:40 -0700

    add Angles

---
+++ JavaTheCompleteReference9thEd/Part-II-TheJavaLibrary/ch17/Angles.java
@@ -0,0 +1,11 @@
+
+class angles {
+  public static void main(String[] args) {
+    double theta = 120.0;
+
+    System.out.println(theta + " degrees is " + Math.toRadians(theta) + " radians");
+
+    theta = 1.312;
+    System.out.println(theta + " degrees is " + Math.toRadians(theta) + " radians");
+  }
+}
