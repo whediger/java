0c8b926b0ee59e1f0dc95761cd4b4044f686d9f2
Wes Hediger <wes.hediger@gmail.com>
2016-12-16 13:36:51 -0700

    add HashSet

---
+++ JavaTheCompleteReference9thEd/Part-II-TheJavaLibrary/ch18/HashSetDemo.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+
+class HashSetDemo {
+  public static void main(String args[])
+  {
+    HashSet<String> hs = new HashSet<String>();
+
+    hs.add("Alpha");
+    hs.add("Beta");
+    hs.add("Eta");
+    hs.add("Gamma");
+    hs.add("Epsilon");
+    hs.add("Omega");
+
+    System.out.println(hs);
+  }
+}
