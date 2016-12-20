2f6af92ad1830a05619af0d37a70307a1822308a
Wes Hediger <wes.hediger@gmail.com>
2016-12-20 13:33:07 -0700

    add ResizeMe

---
+++ JavaTheCompleteReference9thEd/Part-II-TheJavaLibrary/ch25/ResizeMe.java
@@ -0,0 +1,35 @@
+
+import java.applet.*;
+import java.awt.*;
+import java.awt.event.*;
+
+/*
+
+  <applet code="ResizeMe" height=200 width=200></applet>
+
+*/
+
+public class ResizeMe extends Applet {
+  final int inc = 25;
+  int max = 500;
+  int min = 200;
+  Dimension d;
+
+  public ResizeMe() {
+    addMouseListener(new MouseAdapter(){
+      public void mouseReleased(MouseEvent me) {
+        int w = (d.width + inc) > max ? min : (d.width + inc);
+        int h = (d.height + inc) > max ? min : (d.height + inc);
+        setSize(new Dimension(w, h));
+      }
+    });
+  }
+
+  public void paint(Graphics g) {
+    d = getSize();
+
+    g.drawLine(0, 0, d.width-1, d.height-1);
+    g.drawLine(0, d.height-1, d.width-1, 0);
+    g.drawLine(0, 0, d.width-1, d.height-1);
+  }
+}
