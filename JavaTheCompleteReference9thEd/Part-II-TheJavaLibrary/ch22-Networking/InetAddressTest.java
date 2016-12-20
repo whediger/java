500972389afebbdac86312c0310c19c227c26f0b
Wes Hediger <wes.hediger@gmail.com>
2016-12-19 21:57:34 -0700

    add InetAddressTest

---
+++ JavaTheCompleteReference9thEd/Part-II-TheJavaLibrary/ch22-Networking/InetAddressTest.java
@@ -0,0 +1,19 @@
+
+import java.net.*;
+
+class InetAddressTest {
+  public static void main(String args[]) throws UnknownHostException
+  {
+    InetAddress Address = InetAddress.getLocalHost();
+    System.out.println(Address);
+
+    Address = InetAddress.getByName("www.HerbSchildt.com");
+    System.out.println(Address);
+
+    InetAddress SW[] = InetAddress.getAllByName("www.nba.com");
+    for(int i = 0; i < SW.length; i++)
+      System.out.println(SW[i]);
+
+
+  }
+}
