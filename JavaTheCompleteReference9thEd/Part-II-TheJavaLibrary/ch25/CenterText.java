e00151125baa11fb16a9ae10310725a0f5b419b0
Wes Hediger <wes.hediger@gmail.com>
2016-12-20 14:27:58 -0700

    add CenterText

---
+++ JavaTheCompleteReference9thEd/Part-II-TheJavaLibrary/ch25/CenterText.java
@@ -0,0 +1,29 @@
+
+import java.applet.*;
+import java.awt.*;
+
+/*
+  <applet code="CenterText" height=100 width=200></applet>
+*/
+
+public class CenterText extends Applet {
+  final Font f = new Font("SansSerif", Font.BOLD, 18);
+
+  public void paint(Graphics g){
+    Dimension d = this.getSize();
+
+    g.setColor(Color.WHITE);
+    g.fillRect(0, 0, d.width, d.height);
+    g.setColor(Color.BLACK);
+    g.setFont(f);
+    drawCenteredString("This is centered", d.width, d.height, g);
+    g.drawRect(0, 0, d.width-1, d.height-1);
+  }
+
+  public void drawCenteredString(String s, int w, int h, Graphics g){
+    FontMetrics fm = g.getFontMetrics();
+    int x = (w - fm.stringWidth(s)) / 2;
+    int y = (fm.getAscent() + (h - (fm.getAscent() + fm.getDescent())) / 2);
+    g.drawString(s, x, y);
+  }
+}
