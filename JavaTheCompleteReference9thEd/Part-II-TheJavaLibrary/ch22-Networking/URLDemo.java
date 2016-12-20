135abf1beb3a452ad452faade6d127f96ec59eae
Wes Hediger <wes.hediger@gmail.com>
2016-12-19 22:30:29 -0700

    add URLDemo

---
+++ JavaTheCompleteReference9thEd/Part-II-TheJavaLibrary/ch22-Networking/URLDemo.java
@@ -0,0 +1,15 @@
+
+import java.net.*;
+
+class URLDemo {
+  public static void main(String args[]) throws MalformedURLException
+  {
+    URL hp =  new URL("http://www.HerbSchildt.com/WhatsNew");
+
+    System.out.println("protocol: " + hp.getProtocol());
+    System.out.println("Port: " + hp.getPort());
+    System.out.println("Host: " + hp.getHost());
+    System.out.println("File: " + hp.getFile());
+    System.out.println("Ext: " + hp.toExternalForm());
+  }
+}
