35a2c07fa5b39c1fc8248d74713b7a0ce47c4460
Wes Hediger <wes.hediger@gmail.com>
2016-12-20 18:43:31 -0700

    add CBGroup

---
+++ JavaTheCompleteReference9thEd/Part-II-TheJavaLibrary/ch26/CBGroup.java
@@ -0,0 +1,43 @@
+
+import java.awt.*;
+import java.awt.event.*;
+import java.applet.*;
+
+/*
+  <applet code="CBGroup" height="200" width="240"></applet>
+*/
+
+public class CBGroup extends Applet implements ItemListener {
+  String msg = "";
+  Checkbox windows, android, solaris, mac;
+  CheckboxGroup cbg;
+
+  public void init(){
+    cbg = new CheckboxGroup();
+    windows = new Checkbox("Windows", cbg, true);
+    android = new Checkbox("Andriod", cbg, false);
+    solaris = new Checkbox("Solaris", cbg, false);
+    mac = new Checkbox("Mac", cbg, false);
+
+    add(windows);
+    add(android);
+    add(solaris);
+    add(mac);
+
+    windows.addItemListener(this);
+    android.addItemListener(this);
+    solaris.addItemListener(this);
+    mac.addItemListener(this);
+  }
+
+  public void itemStateChanged(ItemEvent ie){
+    repaint();
+  }
+
+  //diaplay curent state of checkboxes
+  public void paint(Graphics g){
+    msg = "Current selection: ";
+    msg += cbg.getSelectedCheckbox().getLabel();
+    g.drawString(msg, 6, 100);
+  }
+}
