89eaecbc20f8b158d5a8bdb91587ce7b72d74993
Wes Hediger <wes.hediger@gmail.com>
2016-12-21 21:52:46 -0700

    add MemoryImageGenerator

---
+++ JavaTheCompleteReference9thEd/Part-II-TheJavaLibrary/ch27/MemoryImageGenerator.java
@@ -0,0 +1,35 @@
+
+/*
+*   <applet code="MemoryImageGenerator" height=700 width=700>
+*   </applet>
+*/
+
+import java.awt.*;
+import java.applet.*;
+import java.awt.image.*;
+
+public class MemoryImageGenerator extends Applet {
+  Image img;
+
+  public void init(){
+    Dimension d = getSize();
+    int w = d.width;
+    int h = d.height;
+    int pixels[] = new int[w * h];
+    int i = 0;
+
+    for (int y = 0; y < h; y++) {
+      for (int x = 0; x < w; x++) {
+        int r = (x^y)&0xff;
+        int g = (x*2^y*2)&0xff;
+        int b = (x*4^y*4)&0xff;
+        pixels[i++] = (255 << 24) | (r << 16) | (g << 8) | b;
+      }
+    }
+    img = createImage(new MemoryImageSource(w, h, pixels, 0, w));
+  }
+
+  public void paint(Graphics g){
+    g.drawImage(img, 0, 0, this);
+  }
+}
