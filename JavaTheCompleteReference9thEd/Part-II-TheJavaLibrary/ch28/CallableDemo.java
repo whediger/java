4db3863abcddc261b1410b63f8aa0eb54e8c2ee8
Wes Hediger <wes.hediger@gmail.com>
2016-12-23 15:25:45 -0700

    add CallableDemo

---
+++ JavaTheCompleteReference9thEd/Part-II-TheJavaLibrary/ch28/CallableDemo.java
@@ -0,0 +1,77 @@
+
+import java.util.concurrent.*;
+
+class CallableDemo {
+  public static void main(String args[])
+  {
+    ExecutorService ex = Executors.newFixedThreadPool(3);
+    Future<Integer> f;
+    Future<Double> f2;
+    Future<Integer> f3;
+
+    System.out.println("Starting");
+
+    f = ex.submit(new Sum(10));
+    f2 = ex.submit(new Hypot(3, 4));
+    f3 = ex.submit(new Factorial(5));
+
+    try{
+      System.out.println(f.get());
+      System.out.println(f2.get());
+      System.out.println(f3.get());
+    }catch(InterruptedException exc){
+      System.out.println(exc);
+    }catch(ExecutionException exc){
+      System.out.println(exc);
+    }
+
+    ex.shutdown();
+    System.out.println("done  +==]========>");
+  }
+}
+
+class Sum implements Callable<Integer>{
+  int stop;
+
+  Sum(int n){
+    stop = n;
+  }
+
+  public Integer call(){
+    int sum = 0;
+    for(int i = 0; i < stop; i++)
+      sum += i;
+    return sum;
+  }
+}
+
+class Hypot implements Callable<Double>{
+  double side1, side2;
+
+  Hypot(double s1, double s2){
+    side1 = s1;
+    side2 = s2;
+  }
+
+  public Double call(){
+    return Math.sqrt((side1*side1) + (side2*side2));
+  }
+}
+
+class Factorial implements Callable<Integer> {
+  int stop;
+
+  Factorial(int n){
+    stop = n;
+  }
+
+  public Integer call(){
+    int fact = 1;
+
+    for (int i = 1; i < stop; i++) {
+      fact *= i;
+    }
+    return fact;
+  }
+
+}
