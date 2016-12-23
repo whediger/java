1e4d38285a59547a7d38fa0f84aed909bdf8247d
Wes Hediger <wes.hediger@gmail.com>
2016-12-23 12:49:26 -0700

    add BarDemo

---
+++ JavaTheCompleteReference9thEd/Part-II-TheJavaLibrary/ch28/BarDemo.java
@@ -0,0 +1,48 @@
+
+import java.util.concurrent.*;
+
+class BarDemo{
+  public static void main(String args[])
+  {
+    CyclicBarrier cb = new CyclicBarrier(3, new BarAction());
+
+    System.out.println("Starting...");
+
+    new MyThread(cb, "A");
+    new MyThread(cb, "B");
+    new MyThread(cb, "C");
+  }
+}
+
+//a thread of exicution that uses CyclicBarrier
+
+class MyThread implements Runnable {
+  CyclicBarrier cbar;
+  String name;
+
+  MyThread(CyclicBarrier c, String n){
+    name = n;
+    cbar = c;
+    new Thread(this).start();
+  }
+
+  @Override
+  public void run(){
+
+    System.out.println(name);
+
+    try{
+      cbar.await();
+    } catch (BrokenBarrierException exc) {
+      System.out.println("Error: Broken Barrier - " + exc);
+    } catch (InterruptedException exc){
+      System.out.println("Error: thread Interrupted - " + exc);
+    }
+  }
+}
+
+class BarAction implements Runnable {
+  public void run(){
+    System.out.println("Barrier Reached!");
+  }
+}
