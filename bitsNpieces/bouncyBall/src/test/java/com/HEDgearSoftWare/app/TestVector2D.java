714e2ee82dfccbfde04dc95c2149713684aa40d0
Wes Hediger <wes.hediger@gmail.com>
2017-01-04 23:58:29 -0700

    add initial mvn test

---
+++ bitsNpieces/bouncyBall/src/test/java/com/HEDgearSoftWare/app/TestVector2D.java
@@ -0,0 +1,17 @@
+package com.HEDgearSoftWare.app;
+
+import org.junit.Test;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+*   bouncyBall is a short exorcise made just for fun
+*/
+
+public class TestVector2D {
+
+  @Test
+  public void test(){
+    fail("not yet implimented");
+  }
+}
